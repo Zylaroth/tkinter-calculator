1a7cbbbb9467b7621cda11222815d37066f6ad50
Zylaroth <pavel2004162@gmail.com>
2023-09-20 20:47:01 +0300

    add buttons

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import tkinter as tk
 
 app = tk.Tk()
-app.title("TEST")
+app.title("Rалькулятор")
 
 display = tk.Entry(app, width=20, font=("Arial", 20))
 display.grid(row=0, column=0, columnspan=4)
+
+buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', 'C', '=', '+'
+]
+
+row_val = 1
+col_val = 0
+
+for button in buttons:
+    tk.Button(app, text=button, width=5).grid(row=row_val, column=col_val)
+    col_val += 1
+    if col_val > 3:
+        col_val = 0
+        row_val += 1
 
 app.mainloop()
