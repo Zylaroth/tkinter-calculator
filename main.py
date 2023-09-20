8e09720661940420f6232e759d7daf861e2a8a47
Zylaroth <pavel2004162@gmail.com>
2023-09-20 21:14:14 +0300

    add logic

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import tkinter as tk
 from tkinter import ttk
 
+def on_button_click(value):
+    if value == "=":
+        try:
+            result = eval(display.get())
+            display.delete(0, tk.END)
+            display.insert(tk.END, str(result))
+        except:
+            display.delete(0, tk.END)
+            display.insert(tk.END, "Ошибка")
+    elif value == "C":
+        display.delete(0, tk.END)
+    else:
+        display.insert(tk.END, value)
+
 app = tk.Tk()
 app.title("Rалькулятор")
 
-display = tk.Entry(app, width=20, font=("Arial", 20))
-display.grid(row=0, column=0, columnspan=4)
-
 style = ttk.Style()
-style.configure("TButton", padding=10, font=("Arial", 16))
+style.configure("TButton", padding=10, font=("Helvetica", 16))
+
+display = tk.Entry(app, width=20, font=("Helvetica", 20))
+display.grid(row=0, column=0, columnspan=4)
 
 buttons = [
     '7', '8', '9', '/',
@@ -21,7 +35,7 @@ row_val = 1
 col_val = 0
 
 for button in buttons:
-    ttk.Button(app, text=button, width=5).grid(row=row_val, column=col_val)
+    ttk.Button(app, text=button, width=5, command=lambda b=button: on_button_click(b)).grid(row=row_val, column=col_val)
     col_val += 1
     if col_val > 3:
         col_val = 0
