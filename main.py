b0f85f2b45feb2806d25f4ed30eb6694ee328d86
Zylaroth <pavel2004162@gmail.com>
2023-09-20 20:48:55 +0300

    add ttk style

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import tkinter as tk
+from tkinter import ttk
 
 app = tk.Tk()
 app.title("Rалькулятор")
 
 display = tk.Entry(app, width=20, font=("Arial", 20))
 display.grid(row=0, column=0, columnspan=4)
+
+style = ttk.Style()
+style.configure("TButton", padding=10, font=("Arial", 16))
 
 buttons = [
     '7', '8', '9', '/',
@@ -17,7 +21,7 @@ row_val = 1
 col_val = 0
 
 for button in buttons:
-    tk.Button(app, text=button, width=5).grid(row=row_val, column=col_val)
+    ttk.Button(app, text=button, width=5).grid(row=row_val, column=col_val)
     col_val += 1
     if col_val > 3:
         col_val = 0
